33ce80f4ef5da7f8e99dfe2bced0d22e2c5121a5
leticiaenz <leticia.enz11@gmail.com>
2020-04-22 21:48:31 -0400

    Create transactions migration and model

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,13 +1,32 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToMany,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @Column()
   category_id: string;
+
+  @ManyToMany(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
 
   created_at: Date;
 
